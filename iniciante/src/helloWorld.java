19822d4b4b1470c777bb014ef013a9150dbc830f
Mirele Loureiro <miloureiro112@gmail.com>
2024-05-11 13:10:54 -0300

    Criando o primeiro programa, o famoso Hello World.

---
+++ iniciante/src/helloWorld.java
@@ -0,0 +1,10 @@
+
+/* O primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+*  Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+*/  
+
+public class helloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

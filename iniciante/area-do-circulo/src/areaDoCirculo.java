30be4ef9559c4cd6b7ad25a6bd74d889a9416e1e
Mirele Loureiro <miloureiro112@gmail.com>
2024-05-11 16:15:53 -0300

    Primeiros exercicios resolvidos

---
+++ iniciante/area-do-circulo/src/areaDoCirculo.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class areaDoCirculo {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio = sc.nextDouble();
+        double n = 3.14159;
+        double area = n * (Math.pow(raio, 2));
+
+        System.out.println("A=" + String.format("%.4f", area));
+        sc.close();
+    }
+}
